487cf470bd977757e1a58492620f33f4381224dd
Zubaidov <shohruz.zubaidov@gmail.com>
2022-12-28 05:46:20 -0800

    another example of decorators

--- decorators.py
+++ decorators.py
@@ -1,12 +1,43 @@
-def hello(name):
-    def greet():
-        print('This is the greet() func inside hello!')
-    def welcome():
-        print('This is welcome() inside the hello')
+# def hello(name):
+#     def greet():
+#         print('This is the greet() func inside hello!')
+#     def welcome():
+#         print('This is welcome() inside the hello')
+#
+#     if name == 'Shohruz':
+#         return greet()
+#     else:
+#         return welcome()
+#
+# #my_new_func = hello('Shohruz')
+#
+#
+# def helllo():
+#     return 'Hi Jose'
+#
+# def other(some_def_func):
+#     print('Ohter CODE runs here')
+#     print(some_def_func())
+#
+# other(helllo)
 
-    if name == 'Shohruz':
-        return greet()
-    else:
-        return welcome()
 
-my_new_func = hello('Shohruz')
+def new_decorator(original_func):
+
+    def wrap_func():
+        print('Some extra code, before the original function')
+
+        original_func()
+
+        print('Some extra code, after the original function')
+
+    return wrap_func
+
+@new_decorator
+def func_needs_decorator():
+    print('I want to be decorated!!')
+
+func_needs_decorator()
+
+# decorated_func = new_decorator(func_needs_decorator)
+# decorated_func()
